5cbc1a973af0a05c780bfa097241d0a9e2162c42
Bohdan Sych <bodya.flesh@gmail.com>
2020-08-29 13:50:22 +0300

    change container docker from mongo to pg

--- task-managment/src/config/typeorm.config.ts
+++ task-managment/src/config/typeorm.config.ts
@@ -3,9 +3,9 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
     host: 'localhost',
-    port: 5432,
-    username: 'postgress',
-    database: 'taskmanagment',
+    port: 5433,
+    username: 'postgres',
+    database: 'postgress',
     entities: [__dirname + '/../**/*.entity.ts'],
     synchronize: true
 };
