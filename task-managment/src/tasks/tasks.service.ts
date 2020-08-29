3dae571469352afc6e013c8845d050fe63da4b2f
Bohdan Sych <bodya.flesh@gmail.com>
2020-08-29 23:47:53 +0300

    relation task

--- task-managment/src/tasks/tasks.service.ts
+++ task-managment/src/tasks/tasks.service.ts
@@ -5,6 +5,7 @@ import { TaskRepository } from './task.repository';
 import { Task } from './task.entity';
 import { TaskStatus } from './task-status.enum';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { User } from 'src/auth/user.entity';
 
 @Injectable()
 export class TasksService {
@@ -28,8 +29,11 @@ export class TasksService {
         return found;
     }
 
-    async createTask(createTaskDto: CreateTaskDto) : Promise<Task>{
-        return this.taskRepository.createTask(createTaskDto);
+    async createTask(
+        createTaskDto: CreateTaskDto,
+        user: User
+    ) : Promise<Task>{
+        return this.taskRepository.createTask(createTaskDto, user);
     }
 
     async deleteTask(id: number): Promise<void>{
